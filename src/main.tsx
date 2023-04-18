8ec34cf06906e6158a4409246040d1df7053c886
storm-legacy <storm.legacy@disroot.org>
2023-04-18 19:08:35 +0200

    fix: bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
