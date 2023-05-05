1e0a44a8b0ebf259bf57e8ea3a67d08b54299cfc
GoldenWindR <kick13322@gmail.com>
2023-05-05 18:25:51 +0200

    Update:Video Add Form fix

--- src/pages/AdminPanel/AdminPages/VideoAdd.tsx
+++ src/pages/AdminPanel/AdminPages/VideoAdd.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 
 export const VideoAdd = () => {
+  const [videoName, setVideoName] = useState('');
   const [videoFile, setVideoFile] = useState('');
+  const [videoAuthor, setVideoAuthor] = useState('');
+  const [videoCategori, setVideoCategori] = useState('');
+  const [videoDiscription, setVideoDiscription] = useState('');
+  const [videoTag, setVideoTag] = useState('');
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
 
     const videoData = {
+      videoName: videoName,
+      videoDiscription: videoDiscription,
+      videoAuthor: videoAuthor,
+      videoTag: videoTag,
+      videoCategori: videoCategori,
       videoFile: videoFile,
     };
     console.log({ videoData });
@@ -19,11 +29,21 @@ export const VideoAdd = () => {
         <form onSubmit={handleSubmit} className="row">
           <label>
             <p>Nazwa</p>
-            <input className="form-control" type="text" />
+            <input
+              className="form-control"
+              type="text"
+              value={videoName}
+              onChange={(event) => setVideoName(event.target.value)}
+            />
           </label>
           <label>
             <p>Opis</p>
-            <input className="form-control" type="text" />
+            <input
+              className="form-control"
+              type="text"
+              value={videoDiscription}
+              onChange={(event) => setVideoDiscription(event.target.value)}
+            />
           </label>
           <label>
             <p>Plik</p>
@@ -31,15 +51,41 @@ export const VideoAdd = () => {
           </label>
           <label>
             <p>Autor</p>
-            <input className="form-control" type="text" />
+            <input
+              className="form-control"
+              type="text"
+              value={videoAuthor}
+              onChange={(event) => setVideoAuthor(event.target.value)}
+            />
           </label>
           <label>
             <p>Kategoria</p>
-            <input className="form-control" type="text" />
+            <input
+              className="form-control"
+              type="list"
+              list="CategoryList"
+              value={videoCategori}
+              onChange={(event) => setVideoCategori(event.target.value)}
+            />
+            <datalist id="CategoryList">
+              <option value="C++" />
+              <option value="Go" />
+              <option value="HowTo" />
+            </datalist>
           </label>
           <label>
             <p>tag</p>
-            <input className="form-control" type="text" />
+            <input
+              className="form-control"
+              list="TagList"
+              value={videoTag}
+              onChange={(event) => setVideoTag(event.target.value)}
+            />
+            <datalist id="TagList">
+              <option value="1" />
+              <option value="2" />
+              <option value="3" />
+            </datalist>
           </label>
           <p></p>
           <button type="submit" className="btn btn-primary ">
