4fbc225273349b6bbc22b6a6e2391b914653b208
GoldenWindR <kick13322@gmail.com>
2023-05-15 22:04:50 +0200

    Update: Video List cross backend

--- src/pages/AdminPanel/AdminPages/VideoList.tsx
+++ src/pages/AdminPanel/AdminPages/VideoList.tsx
@@ -1,44 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import videoService from '../../../services/video.service';
 export const VideoList = () => {
-  const VideoListItem = [
-    {
-      id: '1',
-      name: 'wideo1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida massa mauris, id ',
-      author: 'domino jachaś',
-      tag: 'Lorem',
-      categori: 'Lorem',
-      file: 'Lorem/Lorem',
-    },
-    {
-      id: '2',
-      name: 'wideo2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida massa mauris, id  ',
-      author: 'Lorens',
-      tag: 'Lorem',
-      categori: 'Lorem',
-      file: 'Lorem/Lorem',
-    },
-    {
-      id: '3',
-      name: 'wideo3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida massa mauris, ',
-      author: 'Lorens',
-      tag: 'Lorem',
-      categori: 'Lorem',
-      file: 'Lorem/Lorem',
-    },
-    {
-      id: '4',
-      name: 'wideo4',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida massa mauris,  ',
-      author: 'Lorens',
-      tag: 'Lorem',
-      categori: 'Lorem',
-      file: 'Lorem/Lorem',
-    },
-  ];
+  interface VideoItemData {
+    id: number;
+    name: string;
+    description: string;
+    category: string;
+    tags: string[];
+  }
+  const [videoListItem, setVideoListItem] = useState<VideoItemData[]>([]);
+  useEffect(() => {
+    const { request } = videoService.takeVideo();
+    request
+      .then((res) => {
+        console.log(res);
+        const Videodata = res.data.map(
+          (Videodata: { id: number; name: string; description: string; category: string; tags: string[] }) => {
+            return {
+              id: Videodata.id,
+              name: Videodata.name,
+              description: Videodata.description,
+              category: Videodata.category,
+              tags: Videodata.tags,
+            };
+          },
+        );
+        setVideoListItem(Videodata);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <div className="text-center">
@@ -48,20 +42,18 @@ export const VideoList = () => {
             <div className="row myRow">
               <div className="col-1 border border-primary">ID</div>
               <div className="col-2 border border-primary">Name</div>
-              <div className="col-2  border border-primary">Author</div>
-              <div className="col-2 border border-primary">Description</div>
-              <div className="col-1  border border-primary">Tag</div>
+              <div className="col-3 border border-primary">Description</div>
+              <div className="col-2  border border-primary">Tag</div>
               <div className="col-2  border border-primary">Category</div>
               <div className="col-2 border border-primary"> Settings</div>
             </div>
-            {VideoListItem.map((item, index) => (
+            {videoListItem.map((item, index) => (
               <div className="row" key={index}>
                 <div className="col-1 border border-primary">{item.id}</div>
                 <div className="col-2 border border-primary">{item.name}</div>
-                <div className="col-2  border border-primary">{item.author}</div>
-                <div className="col-2 border border-primary">{item.description}</div>
-                <div className="col-1  border border-primary">{item.tag}</div>
-                <div className="col-2  border border-primary">{item.categori}</div>
+                <div className="col-3 border border-primary">{item.description}</div>
+                <div className="col-2  border border-primary">{item.tags}</div>
+                <div className="col-2  border border-primary">{item.category}</div>
                 <div className="col-2 border border-primary">
                   {' '}
                   Delete <Link to={'/VideoEdit/' + item.id}>Edit</Link> Ban
